691dd5b39bd99af3b32f0a0b9663a573ac815cf1
HILAIRE Richard <silveric@users.noreply.github.com>
2025-04-13 11:22:11 -0400

    Adding handling of characters in the localstorage
    Creating new, loading and deleting.

--- dnd_char_sheet.js
+++ dnd_char_sheet.js
@@ -24,6 +24,7 @@ function init(){
 	console.log(currentCharData);
 	console.log("END-INIT");
 
+	init_characters_storage_datas();
 	displayCharacterInfos();
 }
 
@@ -83,12 +84,20 @@ function loadCharDatas(charID){
 	return null;
 }
 
-function generateRanCharID(){
+function generateRanCharID(security = 0){
+	if(security > 50){return false;}
 	const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	let result = '';
 	for (let i = 0; i < 10; i++) {
 		result += chars.charAt(Math.floor(Math.random() * chars.length));
 	}
+
+	//Checking if that ID isn't already taken
+	let charactersDatas = JSON.parse(localStorage.getItem(localStorageID_charactersDatas));
+	if(charactersDatas[result]){
+		return generateRanCharID(security++);
+	}
+
 	return result;
 }
 
@@ -131,6 +140,74 @@ jQuery(document).on("click","#action_nuke_datas",function() {
 		location.reload();
 	}
 });
+
+function init_characters_storage_datas(){
+	jQuery("#modal_characters_data_characters_list").empty();
+
+	//Getting characters in storage and creating new .modal_characters_data_character_elt in the characters datas modal
+	const charactersDatas = JSON.parse(localStorage.getItem(localStorageID_charactersDatas));
+	console.log(charactersDatas);
+
+	if(typeof charactersDatas !== "undefined"){
+		for (let key in charactersDatas) {
+			const character = charactersDatas[key];
+			const characterID = key;
+
+			const characterEltDOM = jQuery("#hidden_elts .modal_characters_data_character_elt").clone();
+			characterEltDOM.attr("data-char_id",characterID);
+
+			characterEltDOM.find(".modal_characters_data_character_elt_name").html(character["basicInfo_name"] ??
+				characterEltDOM.find(".modal_characters_data_character_elt_name").attr("data-default_val"));
+
+			characterEltDOM.find(".modal_characters_data_character_elt_class").html(character["basicInfo_class"] ??
+				characterEltDOM.find(".modal_characters_data_character_elt_class").attr("data-default_val"));
+
+			characterEltDOM.find(".modal_characters_data_character_elt_lvl").html("["+character["lvl_value"]+"]" ??
+				characterEltDOM.find(".modal_characters_data_character_elt_lvl").attr("data-default_val"));
+
+			characterEltDOM.find(".modal_characters_data_character_elt_species").html(character["basicInfo_species"] ??
+				characterEltDOM.find(".modal_characters_data_character_elt_species").attr("data-default_val"));
+
+			jQuery("#modal_characters_data_characters_list").append(characterEltDOM);
+		}
+	}
+
+	jQuery(document).on("click",".modal_characters_data_character_elt_actions_load",function() {
+		const char_elt_DOM = jQuery(this).parents(".modal_characters_data_character_elt");
+		const characterID = char_elt_DOM.attr("data-char_id");
+
+		localStorage.setItem(localStorageID_lastCharID, characterID);
+		location.reload();
+	});
+
+	jQuery(document).on("click",".modal_characters_data_character_elt_actions_delete",function() {
+		const char_elt_DOM = jQuery(this).parents(".modal_characters_data_character_elt");
+		const characterID = char_elt_DOM.attr("data-char_id");
+
+		let resConfirm = confirm("Are you sure? This will delete the character.");
+		if(!resConfirm){return false;}
+
+		if(characterID === currentCharID){
+			localStorage.removeItem(localStorageID_lastCharID);
+		}
+		//Deleting character from the localstorage
+		let charactersDatas = JSON.parse(localStorage.getItem(localStorageID_charactersDatas));
+		if(!charactersDatas){charactersDatas = {};}
+		delete charactersDatas[currentCharID];
+		localStorage.setItem(localStorageID_charactersDatas, JSON.stringify(charactersDatas));
+		location.reload();
+	});
+
+	jQuery(document).on("click","#action_create_new_character",function() {
+		const newCharID = generateRanCharID();
+		let charactersDatas = JSON.parse(localStorage.getItem(localStorageID_charactersDatas));
+		charactersDatas[newCharID] = {};
+
+		localStorage.setItem(localStorageID_charactersDatas, JSON.stringify(charactersDatas));
+		localStorage.setItem(localStorageID_lastCharID, newCharID);
+		location.reload();
+	});
+}
 
 jQuery(document).on("click",".modal_close",function() {
 	const parentModalDOM = jQuery(this).parents(".modal");
